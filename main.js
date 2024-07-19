7d022f6366358349a42f67ea9e1c9634b6e58a86
Julio Alexandro Gutierrez Poumian <apoumian@192.168.68.61>
2024-07-19 13:38:00 -0600

    Cambio listo

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ function calAge(ageParam) {
   console.log(Math.abs(ageParam - 2024));
 }
 calAge(2000);
+
+function printSomething(somethingParam) {
+  console.log(somethingParam);
+}
+
+printSomething("Imprimiendo algo");
